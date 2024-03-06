d68bc6e1be1c662d9ef65627476c7cf202fb29f7
suzu6331 <128467874+suzu6331@users.noreply.github.com>
2024-03-06 16:53:22 +0900

    Add files via upload

--- httpd.ps1
+++ httpd.ps1
@@ -19,10 +19,52 @@ function CheckServer($server)
 	return $false
 }
 
+#ログインする
 function Login
 {
 	Param($Server, $Profile)
-
+	# ログインAPIに対してPOSTを送信
 	$response = Invoke-RestMethod -Uri "http://$($Server)/login.php" -Method Post -ContentType "applic.ation/json" -InFile $Profile -SessionVariable script:session
-	return ($response.status -eq 200)
+	return ($response.status -eq 200)  #ステータスが200ならログイン成功
+}
+
+#サーバーにファイル送信する
+function SendFile
+{
+    Param($Server, $File)
+    # ファイル更新APIに対してPOSTを送信
+    $response = Invoke-RestMethod -Uri "http://$($Server)/update.php" -Method Post -ContentType "application/json" -InFile $File -WebSession $script:session
+    return ($response.status -eq 204)  #ステータス204なら送信成功
+}
+
+#タイムスタンプの取得する
+function GetTimestamp
+{
+	$filepath = (Join-Path (Get-Location) "user/timestamp.txt")
+	if ([IO.File]::Exists($filepath)) {
+		$t = Get-Content $filepath
+	} else {
+		$t = "00000000000000"  #ファイルが存在しない時はデフォルトの値
+	}
+	return $t
+}
+
+#タイムスタンプ更新
+function UpdateTimestamp
+{
+    $filepath = (Join-Path (Get-Location) "user/timestamp.txt")
+    Get-Date -Format "yyyyMMddHHmmss" > $filepath
+}
+
+#ファイルをアップロード
+function Upload
+{
+	Param()
+}
+
+# HTTP
+try{
+    
+} catch {
+    Write-Error $_.Exception
 }
